927b28315b403dfbfb3dfd1b232cc91343bcd830
gaaldvd <gaaldavid@tuta.io>
2024-08-10 20:50:55 +0200

    files merged

--- libgen_search.py
+++
@@ -1,4 +0,0 @@
-# Handle search requests
-
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
---
+++ libgen_tools.py
@@ -0,0 +1,13 @@
+# Handle search requests and manage results
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+class SearchRequest:
+    pass
+
+class Entry:
+    pass
+
+class Results:
+    pass
