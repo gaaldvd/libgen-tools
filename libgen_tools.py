574091fda2958f935d9beeac844ca40e20ef5a22
gaaldvd <gaaldavid@tuta.io>
2024-08-11 21:37:06 +0200

    search request init basics

--- libgen_tools.py
+++ libgen_tools.py
@@ -3,11 +3,27 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 class SearchRequest:
+
+    url_base = "https://www.libgen.is/search.php?column=def&req="
+
+    def __init__(self, author=None, title=None):
+        self.author = author
+        self.title = title
+        if author and title:
+            self.request_url = f"{self.url_base}{author.replace(" ", "+")}+{title.replace(" ", "+")}"
+        elif author:
+            self.request_url = f"{self.url_base}{author.replace(" ", "+")}"
+        elif title:
+            self.request_url = f"{self.url_base}{title.replace(" ", "+")}"
+
     pass
+
 
 class Entry:
     pass
+
 
 class Results:
     pass
