2512f5b882ea8d6a768d9f70e885443a5b574fbe
gaaldvd <gaaldavid@tuta.io>
2024-08-25 21:59:32 +0200

    work on filtering begun

--- output.py
+++ output.py
@@ -1,21 +1,52 @@
 import ast
 
-entries = []
+FILTERS = {'-a': "auth",
+           '-t': "title",
+           '-y': "year",
+           '-l': "lang",
+           '-e': "ext"}
 
-with open("table", "r") as f:
-    for x in f:
-        entries.append(ast.literal_eval(x))
 
-print("\nNo.".ljust(5) + "Author".ljust(24) + "Title".ljust(34)
-      + "Year".ljust(5) + "Pages".ljust(9) + "Extension\n".ljust(10)
-      + "-" * 85)
-for entry, i in zip(entries, range(len(entries))):
-    auth = entry['auth']
-    title = entry['title']
-    year = entry['year']
-    pp = entry['pp']
-    ext = entry['ext']
-    print(f"{i + 1:<4}"
-          f" {auth[:20] + '...' if len(auth) > 20 else auth:<23}"
-          f" {title[:30] + '...' if len(title) > 30 else title:<33}"
-          f" {year:<4} {pp[:8]:<8} {ext:<9}")
+def list_entries(entries):
+    print("\nNo.".ljust(5) + "Author".ljust(24) + "Title".ljust(34)
+          + "Year".ljust(5) + "Pages".ljust(9) + "Extension\n".ljust(10)
+          + "-" * 85)
+    for entry, i in zip(entries, range(len(entries))):
+        auth = entry['auth']
+        title = entry['title']
+        year = entry['year']
+        pp = entry['pp']
+        ext = entry['ext']
+        print(f"{i + 1:<4}"
+              f" {auth[:20] + '...' if len(auth) > 20 else auth:<23}"
+              f" {title[:30] + '...' if len(title) > 30 else title:<33}"
+              f" {year:<4} {pp[:8]:<8} {ext:<9}")
+
+
+def filter_entries(filters, entries):
+    print("\nfilters:")
+    for key, value in zip(filters.keys(), filters.values()):
+        print(f"  {FILTERS[key]}: {value}")
+    filtered_results = entries
+    for key, value in zip(filters.keys(), filters.values()):
+        filtered_results = [entry for entry in filtered_results if entry[FILTERS[key]] == value]
+    print(f"filtered results: {len(filtered_results)}")
+    return filtered_results
+
+
+def main():
+    entries = []
+    with open("table", "r") as f:
+        for x in f:
+            entries.append(ast.literal_eval(x))
+
+    if input("list entries? ") in ("y", "Y"):
+        list_entries(entries)
+
+    filters = {'-a': "Jane Austen", '-e': "pdf"}
+    # filtered_results = filter_entries(filters, entries)
+    list_entries(filter_entries(filters, entries))
+
+
+if __name__ == '__main__':
+    main()
